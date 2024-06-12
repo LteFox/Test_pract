13e523e4f38cd10afb5aa3c3eb1f8845240c94ac
Dumi <dumidumitrita05@gmail.com>
2024-06-12 15:51:29 +0300

    bad contact

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -45,36 +45,6 @@
     
     <footer class="mt-5 text-white d-flex align-items-center justify-content-center  ">
         <div class="d-flex flex-column">
-          <nav class="navbar navbar-expand-lg  " data-bs-theme="dark">
-            <div class="container ">
-                <a class="navbar-brand" href="{{ route('guest.index') }}">
-                    <img src="{{ asset('assets/images/MilavLogoWhiteSmall2.png') }}" alt="Logo" class="logo">
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-between" id="navbarNavAltMarkup">
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    </form>
-                    <div class="navbar-nav">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        <a class="nav-link active" href="#">About</a>
-                        <a class="nav-link active" href="#">Gallery</a>
-                        <a class="nav-link active" href="#">Contact us</a>
-                        <a href="/login" class="btn btn-bg-light text-white px-4 py-2 me-3">
-                            <i class=""></i>
-                            Log In
-                        </a>
-                        <a href="/register" class="btn btn-bg-dark border text-white px-4 py-2">
-                            <i class=""></i>
-                            Register
-                        </a>
-                        
-                    </div>
-                </div>
-            </div>
-          </nav>
             <div class="container footer-contact-section ">
                 <div class="phone">
                   <hr class="line">
