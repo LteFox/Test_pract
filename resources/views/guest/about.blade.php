57fbb1cf0d6b3633f8021a293e68499029537378
Dumi <dumidumitrita05@gmail.com>
2024-06-14 18:21:13 +0300

    ne bei pj

--- resources/views/guest/about.blade.php
+++ resources/views/guest/about.blade.php
@@ -3,71 +3,31 @@
 @section('content')
 <!-- Header -->
 <div class="bg_image">
-<header>
-    <!-- <div class="container align-items-center justify-content-between top_content">
-        <div class="col-lg-6">
-            <h1 class="text-center text-lg-start text-white mb-5 header-title">
-                !-- <span class="text-site-gradient">NFTs</span> --
-            </h1>
-            <p class="text-light mb-5 text-center text-lg-start header-description">
-            If you expect your software to be worked with hand in hand with your best idea
-            </p>
-            <div class="d-flex justify-content-center">
-                <a href="/login" class="btn btn-bg-light btn-lg text-white px-4 py-2 me-3">
-                    <i class=""></i>
-                    Log In
-                </a>
-                <a href="/register" class="btn btn-bg-dark border btn-lg  text-white px-4 py-2">
-                    <i class=""></i>
-                    Register
-                </a>
-            </div>
-        </div>
-    </div> -->
-</header>
-
-<!-- End Header -->
 <!-- About -->
 <section class=" my-lg-2">
     <div class="container title d-flex flex-column justify-content-between align-items-center flex-lg-row my-5 mb-0">
-        <h2 class="text-white">About Them
+        <h2 class="text-white">Despre Milav
         </h2>
-        <a href="#" class="btn btn-bg-light text-white px-4 py-2 me-3">
-            <i class="fa-solid fa-book-open"></i>
-            Explore All
-        </a>
     </div>
-
-    <div class=" container d-flex  justify-content-between align-items-center flex-lg-column my-5">
-        <div class="  d-flex  justify-content-between align-items-center flex-lg-row my-5">
-            <div class="image-container col-lg-6">
-                <img src="{{ asset('assets/images/aboutUs/about.jpg') }}" alt="About Us Image" class="img-fluid">
-            </div>
-            <div class="content-info">
-                <p class="text-white">MILAV este o companie de dezvoltare software cu sediul în Moldova (Cahul), România. Ea lucrează în principal cu clienți străini și îi ajută să dezvolte soluții software care să le transforme ideile într-un produs digital gata.
-
-                    Unele dintre produsele create pentru clienții săi includ site-uri web corporative, magazine online, portaluri uriașe, aplicații web și pentru întreprinderi, aplicații mobile, dezvoltare full-stack și componente web reutilizabile.
-
-                    MILAV creează produse premiate care transformă afacerile clienților săi. Și este bucuroasă să anunțe că, datorită creșterii continue, are sloturi deschise pentru dezvoltatorii React/React Native medii și seniori.
-                </p>
-                <a href="#" class="btn btn-bg-light text-white px-4 py-2 me-3">More info ---></a>
-            </div>
-
+    <div class="container title d-flex flex-column justify-content-start align-items-center flex-lg-row my-5 mb-0">
+        <div class="col-12 col-lg-6 mb-4 mb-lg-0">
+            <p class="text-white">MILAV este o companie de dezvoltare software cu sediul în Moldova (Cahul), România. Ea lucrează în principal cu clienți străini și îi ajută să dezvolte soluții software care să le transforme ideile într-un produs digital gata.
+                <br>  MILAV creează produse premiate care transformă afacerile clienților săi. Și este bucuroasă să anunțe că, datorită creșterii continue, are sloturi deschise pentru dezvoltatorii React/React Native medii și seniori.
+            </p>
+            <div class="list">
+                <h2>Unele dintre produsele create pentru clienți</h2>
+                <ul>
+                  <li><span>Site-uri web corporative</span></li>
+                  <li><span>Magazine online</span></li>
+                  <li><span>Portaluri uriașe</span></li>
+                  <li><span>Aplicații web și pentru întreprinderi</span></li>
+                  <li><span>Aplicații mobile</span></li>
+                  <li><span>Dezvoltare full-stack și componente web reutilizabile</span></li>
+                </ul>
+              </div>
         </div>
-
-        <div class="icon-container d-flex mt-5 ">
-            <div class="icon-item me-4 mb-4 ">
-                <img src="{{ asset('assets/images/aboutUs/1.svg') }}" alt="Icon 1" class="img-fluid">
-            </div>
-            <div class="icon-item me-4  mb-4">
-                <img src="{{ asset('assets/images/aboutUs/2.svg') }}" alt="Icon 2" class="img-fluid">
-            </div>
-            <div class="icon-item me-4  mb-4">
-                <img src="{{ asset('assets/images/aboutUs/3.svg') }}" alt="Icon 3" class="img-fluid">
-            </div>
-            <div class="icon-item  me-4 mb-4">
-                <img src="{{ asset('assets/images/aboutUs/4.svg') }}" alt="Icon 4" class="img-fluid">
-            </div>
+        <div class="col-12 col-lg-6">
+            <img src="{{ asset('assets/images/img1.svg') }}" alt="About Us Image" class="img-fluid">
         </div>
     </div>
 
@@ -78,67 +38,7 @@
 <!-- Gallery -->
 <section class="collections my-5 py-5">
     <div class="container title d-flex flex-column justify-content-between align-items-center flex-lg-row my-5">
-        <h2 class="text-white text-center text-lg-start">Gallery</h2>
-        <a href="#" class="btn btn-bg-light text-white px-4 py-2 me-3">
-            <i class="fa-regular fa-images"></i>
-            Explore All
-        </a>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <div class="card bg-card radius">
-                    <div class="card-body">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card bg-card radius">
-                    <div class="card-body">
-                        <div class="d-flex flex-wrap">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                        </div>
-                        <div class="d-flex justify-content-between mt-3">
-                            <div class="d-flex author flex-row align-items-center">
-                                <div class="d-flex flex-column justify-content-center">
-                                    <h5 class="text-white mb-0">Metaverse</h5>
-                                    <p class="text-secondary mb-0">Created by <span class="text-white">John Doe</span></p>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card bg-card radius">
-                    <div class="card-body">
-                        <div class="d-flex flex-wrap">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                        </div>
-                        <div class="d-flex justify-content-between mt-3">
-                            <div class="d-flex author flex-row align-items-center">
-
-                                <div class="d-flex flex-column justify-content-center">
-                                    <h5 class="text-white mb-0">Metaverse</h5>
-                                    <p class="text-secondary mb-0">Created by <span class="text-white">John Doe</span></p>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <h2 class="text-white text-center text-lg-start">Developers</h2>
     </div>
 </section>
-<!-- End Gallery -->
-<!-- End Contact Us -->
 @endsection
--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -16,19 +16,16 @@
         </div>
         <div class="service-bloc container d-flex justify-content-center mt-5">
             <div class="bloc me-3 text-center col-3">
-                <div class="circl"><i class="fa-solid fa-brain"></i></div>
                 <h3>UI/UX </h3>
                 <h3>DESIGN</h3>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
             </div>
             <div class="bloc me-3 text-center col-3">
-                <div class="circl"><i class="fa-solid fa-database"></i></div>
                 <h3>DATA</h3>
                 <h3>BASE</h3>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
             </div>
             <div class="bloc text-center col-3">
-                <div class="circl"><i class="fa-solid fa-server"></i></div>
                 <h3>HOSTING</h3>
                 <h3>SEO</h3>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
@@ -37,12 +34,26 @@
     </header>
     <!-- End Header -->
     <!-- About -->
+    <section class="my-lg-2">
+        <div class="container title d-flex flex-column justify-content-start align-items-center flex-lg-row my-5 mb-0">
+            <div class="col-12 col-lg-6 mb-4 mb-lg-0">
+                <h2 class="text-white">Cu ce ne ocupăm?</h2>
+                <p class="text-white">
+                    Suntem o echipa de profesionisti cu capacitățile necesare pentru a crea orice proiect, indiferent de complexitatea acestuia începând cu site-uri simple corporative și finisând cu magazine online integrate cu sisteme de plată.
+                    <br>De-a lungul anilor, am oferit partenerilor noștri servicii de externalizare de înaltă calitate și împreună am dezvoltat produse premiate.
+                </p>
+            </div>
+            <div class="col-12 col-lg-6">
+                <img src="{{ asset('assets/images/img3.1.svg') }}" alt="Tipuri de site-uri web" class="img-fluid rounded">
+            </div>
+        </div>
+    </section>
+    
     <section class=" my-lg-2">
         <div class="container title d-flex flex-column justify-content-center align-items-center text-center flex-lg-row my-5 mb-0">
             <div class="col-6">
                 <h2 class="text-white">Tipuri de site-uri web
                 </h2>
-                <p class="text-white">Suntem o echipa de profesionisti cu capacitățile necesare pentru a crea orice proiect, indiferent de complexitatea acestuia începând cu site-uri simple corporative și finisând cu magazine online integrate cu sisteme de plată. Mai jos am descris pe scurt tipurile standarte de website-uri pe care putem sa le elaboram pentru dvs.</p>
             </div>
         </div>
 
@@ -85,7 +96,7 @@
                     <div class="shad mb-2 mt-2"><i class="fa-solid fa-blog frimeworck"></i></div>
                     <H4>Blog</H4>
                     <p>Bloguri personale sau tematice. Devin-o un scriitor mai bun sau un expert în domeniul tău.</p>
-                    <p>350<i class="fa-solid fa-euro-sign"></i></p>
+                    {{-- <p>350<i class="fa-solid fa-euro-sign"></i></p> --}}
                 </div>
 
             </div>
@@ -93,16 +104,6 @@
         
     </section>
 </div>
-<div class="swiper auctionsSwiper container">
-            <div class="swiper-wrapper">
-                <div class="swiper-slide"><p><i class="fa-brands fa-laravel"></i></p></div>
-                <div class="swiper-slide"><p><i class="fa-brands fa-html5"></i></p></div>
-                <div class="swiper-slide"><p><i class="fa-brands fa-css3-alt"></i></p></div>
-                <div class="swiper-slide"><p><i class="fa-brands fa-square-js"></i></p></div>
-                <div class="swiper-slide"><p><i class="fa-brands fa-php"></i></p></div>
-                <div class="swiper-slide"><p class="MySQL">MySQL</p></div>
-            </div>
-        </div>
 <!-- End About Them -->
 <!-- Gallery -->
 <section class="collections my-5 py-5">
@@ -111,75 +112,43 @@
     </div>
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
+            @foreach($collections as $collection)
+                <div class="col-md-4">
                 <div class="card bg-card radius">
                     <div class="card-body">
-                        <div class="d-flex flex-wrap">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
+                        <div class="d-flex flex-wrap images">
+                            @foreach($collection->images()->limit(4)->get() as $image)
+                                <img src="{{ asset(env('UPLOADS_IMAGE'). "/" . $image->name) }}" class="w-100 p-2 radius" alt="Metaverse">
+                            @endforeach
                         </div>
                         <div class="d-flex justify-content-between mt-3">
                             <div class="d-flex author flex-row align-items-center">
                                 <div class="d-flex flex-column justify-content-center">
-                                    <h5 class="text-white mb-0">Metaverse</h5>
-                                    <p class="text-secondary mb-0">Created by <span class="text-white">John Doe</span></p>
+                                    <h5 class="text-white mb-0">{{ $collection->name }}</h5>
+                                    <p class="text-secondary mb-0">Created by <span class="text-white">{{ $collection->user()->get()->first()->name }}</span></p>
                                 </div>
                             </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card bg-card radius">
-                    <div class="card-body">
-                        <div class="d-flex flex-wrap">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                        </div>
-                        <div class="d-flex justify-content-between mt-3">
-                            <div class="d-flex author flex-row align-items-center">
-                                <div class="d-flex flex-column justify-content-center">
-                                    <h5 class="text-white mb-0">Metaverse</h5>
-                                    <p class="text-secondary mb-0">Created by <span class="text-white">John Doe</span></p>
-                                </div>
+                            <div class="d-flex flex-column justify-content-center">
+                                <i class="fa-regular fa-heart text-white text-center like-button" data-collection-id="{{ $collection->id }}"></i>
+                                <span class="text-white text-center like" >{{ $collection->likes }}</span>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
-                <div class="card bg-card radius">
-                    <div class="card-body">
-                        <div class="d-flex flex-wrap">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                            <img src="{{ asset('assets/images/gallery/im1.png') }}" class="w-50 p-2 radius" alt="Metaverse">
-                        </div>
-                        <div class="d-flex justify-content-between mt-3">
-                            <div class="d-flex author flex-row align-items-center">
-
-                                <div class="d-flex flex-column justify-content-center">
-                                    <h5 class="text-white mb-0">Metaverse</h5>
-                                    <p class="text-secondary mb-0">Created by <span class="text-white">John Doe</span></p>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </section>
-<!-- End Gallery -->
-<!-- Contact Them -->
-<section class=" my-5 py-5">
-</section>
-<!-- End Contact Us -->
+
+<div class="swiper auctionsSwiper container">
+    <div class="swiper-wrapper">
+        <div class="swiper-slide"><p><i class="fa-brands fa-laravel"></i></p></div>
+        <div class="swiper-slide"><p><i class="fa-brands fa-html5"></i></p></div>
+        <div class="swiper-slide"><p><i class="fa-brands fa-css3-alt"></i></p></div>
+        <div class="swiper-slide"><p><i class="fa-brands fa-square-js"></i></p></div>
+        <div class="swiper-slide"><p><i class="fa-brands fa-php"></i></p></div>
+        <div class="swiper-slide"><p class="MySQL">MySQL</p></div>
+    </div>
+</div>
 @endsection
